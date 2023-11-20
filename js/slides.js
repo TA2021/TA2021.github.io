05eb12ed235f9deb9ca31ca14d092cbefbe634c9
Tim Adams <zfac125@live.rhul.ac.uk>
2023-11-20 19:07:36 +0000

    added functionality to the 3D carousel

--- js/slides.js
+++ js/slides.js
@@ -38,3 +38,26 @@ nextButton.addEventListener( 'click', function() {
   selectedIndex++;
   rotateCarousel();
 });
+
+let isDragging = false;
+let initialX;
+
+//const carousel = document.getElementById('carousel');
+
+carousel.addEventListener('touchstart', (e) => {
+  isDragging = true;
+  initialX = e.touches[0].clientX;
+});
+
+carousel.addEventListener('touchmove', (e) => {
+  if (!isDragging) return;
+
+  const deltaX = e.touches[0].clientX - initialX;
+  const rotation = (deltaX / carousel.clientWidth) * 360;
+
+  carousel.style.transform = `rotateY(${rotation}deg)`;
+});
+
+carousel.addEventListener('touchend', () => {
+  isDragging = false;
+});
